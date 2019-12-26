93c055e03aeba32743af6bcfc7b736a89d30b5ee
Lucas Santi <idlucassanti@hotmail.com>
2019-12-26 20:15:42 -0300

    Criando o icon com TemplateString e inserindo no memoryCard

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-$root = document.querySelector("#root");
+const $root = document.querySelector("#root");
 
-$memoryCard  = document.createElement("article");
+const $memoryCard  = document.createElement("article");
 $memoryCard.classList.add("memory-card");
 
+const $icon = `<img src="img/icon-collabcode.png" alt="" class="icon">`;
+
+$memoryCard.innerHTML = $icon;
+
 $root.insertBefore($memoryCard, null);
-
-
