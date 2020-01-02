b01f38b8485165d4ee29a2619e9c5dbc08cbcebf
Santi L <idlucassanti@hotmail.com>
2020-01-02 12:58:28 -0300

    organizando js - isolando pages e components

---
+++ src/components/memory-card/index.js
@@ -0,0 +1,15 @@
+function createMemoryCard() {
+
+    const $memoryCard  = document.createElement("article");
+
+    $memoryCard.classList.add("memory-card");
+
+    const $icon = `
+        <img src="img/icon-collabcode.png" alt="" class="icon"/>
+    `;
+
+    $root.insertBefore($memoryCard, null);
+
+    $memoryCard.insertAdjacentHTML("beforeend", $icon);
+
+}
