16a0d817477c855f938e8f76cb87c3a32f8cafb9
Santi L <idlucassanti@hotmail.com>
2020-01-06 17:30:41 -0300

    evento em Elements

--- src/objects/wrap-cards/index.js
+++ src/objects/wrap-cards/index.js
@@ -2,5 +2,9 @@ function createCardsWrapper(){
     const $cardsWrapper = document.createElement("section");
     $cardsWrapper.classList.add("wrap-cards");
 
+    $cardsWrapper.addEventListener("click", ()=>{
+        console.log('eai');
+    });
+
     return $cardsWrapper;
 }
