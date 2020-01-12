faee24c0c104dd42910e9468837293c6c71a11c4
Santi L <idlucassanti@hotmail.com>
2020-01-12 12:11:49 -0300

    css e js no mesmo arquivo - dentro de uma funcao

--- src/objects/wrap-cards/index.js
+++ src/objects/wrap-cards/index.js
@@ -1,6 +1,25 @@
 function createCardsWrapper(){
     const $cardsWrapper = document.createElement("section");
     $cardsWrapper.classList.add("wrap-cards");
-    
+
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+    const style = `
+        .wrap-cards{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-evenly;
+            padding-top: 10px;
+        }
+        
+        .wrap-cards > .memory-card{
+            margin-bottom: 10px;
+        }
+    `;
+
+    $style.textContent = style;
+
+    $head.insertAdjacentElement("beforeend", $style);
+
     return $cardsWrapper;
 }
