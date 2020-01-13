4f5e89b1a91a683237c20021148f3a0b4229de2b
Santi L <idlucassanti@hotmail.com>
2020-01-13 18:19:13 -0300

    função retornando função - function expression

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,22 @@
-const getName = () => "Lucas Santi"
+let infos = {};
 
-const name = getName();
+function imprimirDados(name, email){
+    console.log("entrei na função");
+    console.log("Name", name);
+    console.log("Email", email);
 
-console.log(name);
+    return function(){
+        infos = {
+            name,
+            email
+        };
+    };
+};
+
+console.log("Infos antes:", infos);
+
+const salvaDados = imprimirDados("Lucas Santi","lucassanti_@hotmail.com");
+
+salvaDados();
+
+console.log("Infos depois", infos);
